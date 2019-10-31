16eda629a94ccfe911124ad6b164ad44b36d3842
Andrey Khiryanov <andreykhiryanov@gmail.com>
2019-10-31 10:02:13 +0300

    First launch

---
+++ src/main/java/com/generation/brainless/hometask02/Start.java
@@ -0,0 +1,11 @@
+package com.generation.brainless.hometask02;
+
+public class Start {
+
+    public static void main(String[] args) {
+
+        System.out.println("!");
+
+    }
+
+}
---
+++ src/main/java/com/generation/brainless/hometask02/config/SpringConfig.java
@@ -0,0 +1,7 @@
+package com.generation.brainless.hometask02.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+}
